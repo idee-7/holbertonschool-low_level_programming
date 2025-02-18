8e9bdfd7c003ae3d28ad71ec37c0499f3a00aca6
Heidi <1016@holbertonstudents.com>
2025-02-18 00:46:30 +0000

    l

---
+++ hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#!/bin/bash
+/*
+ * File: 4-puts.c
+ * Auth: Heidi Leeman <10516@holbertonschool.com>
+ * */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual puzzle" followed by a new line
+ *
+ * Return: Always 0 
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle")'
+
+    return (0);
+}
