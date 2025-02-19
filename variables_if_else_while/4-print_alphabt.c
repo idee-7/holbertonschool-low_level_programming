b4e32411338af9d658ca05129bdf8d932dfddec9
Heidi <10516@holbertonstudents.com>
2025-02-19 23:50:08 +0000

    k

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+/*
+ * File: 4-print_alphabt.c
+ * Auth: Heidi Leeman
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if(letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
