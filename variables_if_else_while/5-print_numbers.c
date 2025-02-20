ea26b8039eb9546d07cb84180c9628f4f5e3f83c
Heidi <10516@holbertonstudents.com>
2025-02-20 00:00:27 +0000

    doing it right

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Heidi Leeman
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+
+	for(num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
