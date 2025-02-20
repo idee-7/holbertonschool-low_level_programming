b2cac70e17412e025433e6df9808dad317cb3e84
Heidi <10516@holbertonstudents.com>
2025-02-20 01:23:05 +0000

    l

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Heidi Leeman
+ */
+
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase followed by a new line.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('n');
+}
