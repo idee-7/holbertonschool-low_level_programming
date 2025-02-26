7024615cc31c97d1437f974db663d22bdfebad1a
idee-7 <10516@holbertonstudents.com>
2025-02-26 20:54:36 +1100

    Create 1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/** 
+ * _isdigit - checks for digit.
+ * @c: character to check.
+ *
+ * Return: 1 when c is digit, 0 otherwise.
+ */
+int _isupper(int c)
+
+{
+	if (c >= 1 && c <= 9)
+		return (1);
+	else
+		return (0);
+
+}
