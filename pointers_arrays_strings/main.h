f667bea5671960cb2a04b071aba90e5b32a32698
idee-7 <10516@holbertonstudents.com>
2025-03-03 23:14:58 +1100

    Update main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,35 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
-/*
- * File: main.h
- * Auth: Heidi Leeman
- * Desc: Header file containing declarations for all functions
- *       used in the 0x02-functions_nested_loops directory.
- */
-
+/**
+* void prototypes()
+*char prototypes()
+* int prototypes()
+*/
+void reset_to_98(int *n);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void swap_int(int *a, int *b);
+void swap_char(char *a, char *b);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-
-#endif
+int _strlen(char *s);
+int _atoi(char *s);
+#endif /*MAIN_H*/
