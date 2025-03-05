29ccd13550146d737c391e25804fac8c25cf8bb8
idee-7 <10516@holbertonstudents.com>
2025-03-05 11:42:15 +1100

    Create 3-strspn.c

---
+++ 0x02-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strspn - returns the number of bytes in the initial segment of s which consists only
+             of bytes from accept
+ *
+ * @char *s:
+ * @char *accept:
+ *
+ * Return: a pointer to @dest
+ */
