df1b47388ab672a983b3f12e52cb94f8ea7a79b4
idee-7 <10516@holbertonstudents.com>
2025-03-05 11:38:18 +1100

    Create 2-strchr.c

---
+++ 0x02-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * char *_strchr - Returns a pointer to the first occurrence of the character @c 
+                   in the string @s, or NULL if the character is not found
+ *
+ * @char c: the character
+ * @char *s: pointer to the string.
+ *
+ * Return: a pointer to the first occurrence of the character @c.
+ */
+
+char *_strchr(char *s, char c)
+{
+}
