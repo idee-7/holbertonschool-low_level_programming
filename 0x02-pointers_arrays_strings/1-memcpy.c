0cce448bb25389a0c7a31644171e050c31734b93
idee-7 <10516@holbertonstudents.com>
2025-03-05 12:03:51 +1100

    Update 1-memcpy.c

--- 0x02-pointers_arrays_strings/1-memcpy.c
+++ 0x02-pointers_arrays_strings/1-memcpy.c
@@ -14,10 +14,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
   unsigned int i = 0;
 
   while (i < n;) 
-    {
-    src[i] = dest[i];
+  {
+    dest[i] = src[i];
     i ++;
-}
+  }
   
   return(dest);
 }
