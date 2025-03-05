a4f2ee8a6f90ce1d3cfb7e751e62c953704b73db
idee-7 <10516@holbertonstudents.com>
2025-03-05 12:29:40 +1100

    Update 3-strspn.c

--- 0x02-pointers_arrays_strings/3-strspn.c
+++ 0x02-pointers_arrays_strings/3-strspn.c
@@ -1,10 +1,32 @@
 #include "main.h"
 /**
- * _strspn - returns the number of bytes in the initial segment of s which consists only
-             of bytes from accept
+ * _strspn - gets the length of a prefix substring.
  *
- * @char *s:
- * @char *accept:
+ * @s: segment to return bytes from
+ * @accept: the bytes to include
  *
- * Return: a pointer to @dest
+ * Return: the number of bytes in the initial segment of @s which consists only
+ * of bytes from @accept pointer 
  */
+
+unsigned int _strspn(char *s, char *accept)
+{
+  int i, j;
+  int c = 0;
+
+  for (i = 0; s[i] != '\0'; i++)
+    {
+      if (s[i] ! = 32
+      {
+        for (j = 0; accept[j] != '\0'; j++)
+          {
+            if (s[i] == accept[j])
+              c++;
+          }
+      }
+      else
+        return (c);
+
+    }
+  return (c);
+}
